7310a51a72f255d8292750424b030bf127c2722e
grigorievk <grigorievka@gmail.com>
2019-12-07 18:59:35 +0300

    Change function to class. Refactoring for render function. Add change task to task-edit view.

--- src/components/button-more.js
+++ src/components/button-more.js
@@ -1,3 +1,27 @@
-export const createButtonLoadMoreTemplate = () => {
+import {createElement} from "../utils";
+
+const createButtonLoadMoreTemplate = () => {
   return `<button class="load-more" type="button">load more</button>`;
 };
+
+export default class LoadMoreButton {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createButtonLoadMoreTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}

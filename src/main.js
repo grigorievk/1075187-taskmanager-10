efe726c2cce9d5af2c4881c28ce9de853b30edee
grigorievk <grigorievka@gmail.com>
2019-12-07 19:32:49 +0300

    Add Esc key event listener. Add empty content message.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import SiteMenuComponent from "./components/menu";
 import FilterComponent from "./components/filter";
 import ContentComponent from "./components/content";
 import FilterBoardComponent from "./components/filter-board";
+import TaskListEmptyComponent from "./components/task-list-empty";
 import TaskEditComponent from "./components/task-edit";
 import TaskComponent from "./components/task";
 import LoadMoreButtonComponent from "./components/button-more";
@@ -17,7 +18,7 @@ const contentComponent = new ContentComponent();
 const taskListElement = contentComponent.getElement().querySelector(`.board__tasks`);
 const TASK_PER_PAGE = 8;
 let currentTaskSlot = 1;
-let taskData = generateTaskData(TASK_PER_PAGE * 3); // generate 3 slots of cards + 1 for first editable card
+let taskData = generateTaskData(TASK_PER_PAGE * 3); // generate 3 slots of cards
 const generateTaskTemplate = (data, count) => {
   new Array(count)
     .fill(``)
@@ -26,19 +27,35 @@ const generateTaskTemplate = (data, count) => {
       const taskEditComponent = new TaskEditComponent(data[i]);
       const editButton = taskComponent.getElement().querySelector(`.card__btn--edit`);
       const editForm = taskEditComponent.getElement().querySelector(`form`);
+      const onEscKeyDown = (evt) => {
+        const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+        if (isEscKey) {
+          replaceEditToTask();
+          document.removeEventListener(`keydown`, onEscKeyDown);
+        }
+      };
+
+      const replaceEditToTask = () => {
+        taskListElement.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
+      };
+
+      const replaceTaskToEdit = () => {
+        taskListElement.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
+      };
 
       editButton.addEventListener(`click`, () => {
-        taskListElement.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
+        replaceTaskToEdit();
+        document.addEventListener(`keydown`, onEscKeyDown);
       });
 
-      editForm.addEventListener(`submit`, () => {
-        taskListElement.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
-      });
+      editForm.addEventListener(`submit`, replaceEditToTask);
 
       return render(taskListElement, taskComponent.getElement(), RenderPosition.BEFOREEND);
     })
     .join(``);
 };
+const isAllTasksArchived = taskData.every((task) => task.isArchive);
 
 render(siteHeaderElement, new SiteMenuComponent().getElement(), RenderPosition.BEFOREEND);
 
@@ -46,23 +63,29 @@ const filterData = getFilterData(taskData);
 
 render(siteMainElement, new FilterComponent(filterData).getElement(), RenderPosition.BEFOREEND);
 render(siteMainElement, contentComponent.getElement(), RenderPosition.BEFOREEND);
-render(contentComponent.getElement(), new FilterBoardComponent().getElement(), RenderPosition.AFTERBEGIN);
 
-generateTaskTemplate(taskData, TASK_PER_PAGE);
+if (isAllTasksArchived) {
+  render(contentComponent.getElement(), new TaskListEmptyComponent().getElement(), RenderPosition.BEFOREEND);
+} else {
+  render(contentComponent.getElement(), new FilterBoardComponent().getElement(), RenderPosition.AFTERBEGIN);
 
-render(contentComponent.getElement(), new LoadMoreButtonComponent().getElement(), RenderPosition.BEFOREEND);
+  generateTaskTemplate(taskData, TASK_PER_PAGE);
 
-document.addEventListener(`click`, (event) => {
-  if (event.target && event.target.classList.contains(`load-more`)) {
-    const dataFrom = currentTaskSlot * TASK_PER_PAGE;
-    const dataTo = dataFrom + TASK_PER_PAGE;
-    const moreTaskData = taskData.slice(dataFrom, dataTo);
+  render(contentComponent.getElement(), new LoadMoreButtonComponent().getElement(), RenderPosition.BEFOREEND);
 
-    generateTaskTemplate(moreTaskData, TASK_PER_PAGE);
-    currentTaskSlot++;
+  document.addEventListener(`click`, (event) => {
+    if (event.target && event.target.classList.contains(`load-more`)) {
+      const dataFrom = currentTaskSlot * TASK_PER_PAGE;
+      const dataTo = dataFrom + TASK_PER_PAGE;
+      const moreTaskData = taskData.slice(dataFrom, dataTo);
 
-    if (dataTo === taskData.length) {
-      event.target.classList.add(`visually-hidden`);
+      generateTaskTemplate(moreTaskData, TASK_PER_PAGE);
+      currentTaskSlot++;
+
+      if (dataTo === taskData.length) {
+        event.target.classList.add(`visually-hidden`);
+      }
     }
-  }
-});
+  });
+}
+
